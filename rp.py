295fd1183d0e49da81f8cb72691c8cda2f5673cc
doremon15 <62587180+doremon15@users.noreply.github.com>
2020-03-24 16:39:43 +0530

    Update rp.py

--- rp.py
+++ rp.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import asynico
+import time
 
 TOKEN = 'NjkxOTM1OTAzODkxMDYyNzk1.XnnPbA.CCbL72vOZApf7J9hvARomFlqzjI'
 
@@ -10,7 +12,7 @@ async def on_ready():
     print('Bot is ready.')
     
 @client.command()
-async def 1():
+async def +1():
   await client.say('1\1\1\1\1\1\1cnf')
   
-  client.run(TOKEN)
+client.run(TOKEN)
